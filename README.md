dd31f401d119618cad9c479f37e64af4de6101ee
Arindam <amusib32@gmail.com>
2024-01-11 22:25:47 +0530

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+#Frontend Develpment with React.js
+#parcel
+-Dev build
+-Local server
+-HMR->Hot Module Replacement
+-File Watching Algorithm->written in c++
+-cahing->faster things
+-image optimization
+-Bundeling
+-code optimization
+-https
+-support to older versions of web browsers
--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-// const heading=React.createElement("h1",{id:"heading"},"Namaste React!");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// const heading=React.createElement("h1",{id:"heading"},"Namaste everyone!");
 // const root=ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
 
-const parent=React.createElement('div',{id:"parent"},
-React.createElement('div',{id:"child"},[React.createElement('h1',{},"I'm h1 tag"),React.createElement("h2",{},"i'm h2 tag")])
+// const parent=React.createElement('div',{id:"parent"},
+// React.createElement('div',{id:"child"},[React.createElement('h1',{},"I'm h1 tag"),React.createElement("h2",{},"i'm h2 tag")])
 
-);
-console.log(parent);
+// );
+// console.log(parent);
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(parent);
+// //using jsx
+// const jsxHeading=<h1 id="heading">Namaste React with JSX</h1>;
+// console.log(jsxHeading);
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(jsxHeading);
+// const jsxHeading=<h1 className='head'>Namaste React with JSX</h1>;
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(jsxHeading);
+
+//react functional component
+const Heading=()=>{
+    return <h1 className='head'>Heading 1</h1>
+};
+//component composition
+const Headingcomponent=()=>{
+    return <div id='container'>
+        <h2>{1+2}</h2>
+        <Heading/>
+    <h1 className='heading'>React Components</h1>
+    </div>
+};
 const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<Headingcomponent/>);
+
+   
